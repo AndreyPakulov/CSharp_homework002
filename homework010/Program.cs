77e349a7054987a3278bd3bc2790da70acdbc923
Andrey Pakulov <anpa1984@yandex.ru>
2022-09-04 22:20:14 +0200

    added homework 010

---
+++ homework010/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(CountSecond(numberA));
+
+int CountSecond(int num1)
+{
+    return (num1 / 10) - (num1 / 100 * 10);
+}

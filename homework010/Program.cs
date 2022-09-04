ff956de92a0f31ddc2268a64b1f64482d2929290
Andrey Pakulov <anpa1984@yandex.ru>
2022-09-04 23:01:19 +0200

    second decision

--- homework010/Program.cs
+++ homework010/Program.cs
@@ -4,11 +4,20 @@
 // 782 -> 8 
 // 918 -> 1
 
+// Второй вариант:
+
 Console.WriteLine("Введите трёхзначное число");
 int numberA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(CountSecond(numberA));
+string numberAText = Convert.ToString(numberA);
+Console.WriteLine($"{"Вторая цифра"} {numberAText[1]}");
 
-int CountSecond(int num1)
-{
-    return (num1 / 10) - (num1 / 100 * 10);
-}
+// Первый вариант:
+
+// Console.WriteLine("Введите трёхзначное число");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(CountSecond(numberA));
+
+// int CountSecond(int num1)
+// {
+//     return (num1 / 10) - (num1 / 100 * 10);
+// }
